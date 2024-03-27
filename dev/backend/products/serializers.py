8a298a01269c0a5efbeda8e21105369f21b961ea
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-03-27 04:51:36 +0000

    using reverse to include dynamic urls

--- dev/backend/products/serializers.py
+++ dev/backend/products/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.HyperlinkedIdentityField(
+        view_name='products-update', lookup_field='pk'
+        )
     class Meta:
         model = Product
-        fields = ['title', 'price', 'content', 'sale_price', 'get_discount']
+        fields = ['url', 'edit_url', 'title', 'price', 'content', 'sale_price', 'get_discount']
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+        return reverse('products-detail', kwargs={'pk': obj.id}, request=request)
+
--- dev/backend/products/urls.py
+++ dev/backend/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListCreateView.as_view()),
-    path('<int:pk>/', views.ProductDetailView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateView.as_view()),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='products-detail'),
+    path('<int:pk>/update/', views.ProductUpdateView.as_view(), name='products-update'),
     path('<int:pk>/delete/', views.ProductDeleteView.as_view()),
 ]
